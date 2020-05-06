0045db452140e2d69e7621d01f13bbef7a6aba73
Jonathan Sandoval Cardona <jsandoval@utp.edu.co>
2020-05-05 20:31:56 -0500

    Create two graphs.

--- parse_results.py
+++ parse_results.py
@@ -1,5 +1,10 @@
+import random
 import subprocess
 from datetime import datetime
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 file_list = [
     "slurm-8919210.out",
@@ -32,16 +37,23 @@ file_list = [
 results = []
 results_dict = {}
 elapsed_dict = {}
-print("file_name;epoch_index;hr_accuracy;train_time;val_time")
+
 for file_name in file_list:
-    process_handle = subprocess.run(["tail", "-n", "2", "input_files/" + file_name], capture_output=True)
+    with open(f"input_files/{file_name}") as f:
+        ngpu = int(f.readline().split()[5])
+
+    process_handle = subprocess.run(
+        ["tail", "-n", "2", "input_files/" + file_name], capture_output=True
+    )
     lines = process_handle.stdout.decode("utf-8")[:-1].split("\n")
 
     start = datetime.strptime(lines[1][-22:], "%Y-%m-%d %H:%M:%S %p")
     end = datetime.strptime(lines[0][-22:], "%Y-%m-%d %H:%M:%S %p")
     delta = end - start
 
-    process_handle = subprocess.run(["grep", "_time", "input_files/" + file_name], capture_output=True)
+    process_handle = subprocess.run(
+        ["grep", "_time", "input_files/" + file_name], capture_output=True
+    )
     epochs = process_handle.stdout.decode("utf-8")[:-1].split("\n")
     # Epoch 0: HR@10 = 0.4211, NDCG@10 = 0.2385, train_time = 2.64, val_time = 0.15
     for epoch in epochs:
@@ -53,11 +65,13 @@ for file_name in file_list:
                 epoch_index = epoch_index.split(" ")[1].strip()
                 hr_accuracy = hr_accuracy.split("=")[1].strip()
             elif value.startswith("train_time"):
-                train_time = value.split("=")[1].strip()
+                train_time = float(value.split("=")[1].strip())
             elif value.startswith("val_time"):
-                val_time = value.split("=")[1].strip()
+                val_time = float(value.split("=")[1].strip())
 
-        results.append([file_name, epoch_index, hr_accuracy, train_time, val_time])
+        results.append(
+            [file_name, int(epoch_index), hr_accuracy, train_time, val_time, ngpu]
+        )
         elapsed_dict[file_name] = [delta, delta.seconds]
 
 for result in results:
@@ -66,17 +80,96 @@ for result in results:
     else:
         results_dict[result[0]] = [result[1:]]
 
-# print("file_name;delta;delta.seconds")
-# for key, value in elapsed_dict.items():
-#     print("%s;%s;%d" % (key, value[0], value[1]))
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
 
+epochs_list = []
+hr_accuracy_list = []
+train_time_list = []
+val_time_list = []
+ngpu_list = []
+
+keys_list = []
+
+counter = 0
 for key, values in results_dict.items():
-    hr_accuracy_list = []
-    train_time_list = []
-    val_time_list = []
     for value in values:
+        epochs_list.append(value[0])
         hr_accuracy_list.append(value[1])
+
         train_time_list.append(value[2])
         val_time_list.append(value[3])
 
-    print(key,hr_accuracy_list ,train_time_list, val_time_list)
+        ngpu_list.append(value[-1])
+
+        keys_list.append(key)
+
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+sns.set(rc={"figure.figsize": (11.7, 8.27)})
+
+df = pd.DataFrame()
+
+# Add columns
+df["epochs"] = epochs_list
+df["train_time"] = train_time_list
+df["ngpu"] = ngpu_list
+df["keys"] = keys_list
+
+sns.lineplot(
+    x="epochs",  # Horizontal axis
+    y="train_time",  # Vertical axis
+    hue="keys",  # Set color
+    data=df,  # Data source
+    estimator=None,
+    lw=1,
+    sort=True,
+    # style="x",
+    #    style="ngpu",
+    #    kind="line",
+    dashes=False,
+)
+
+# Set title
+plt.title("Training times accross epochs.")
+
+# Set x-axis label
+plt.xlabel("epochs")
+
+# Set y-axis label
+plt.ylabel("time")
+
+plt.savefig("train_time.png")
+# It can be plt.show() but it requires tkinter.
+
+
+df["epochs"] = epochs_list
+df["valid_time"] = val_time_list
+df["ngpu"] = ngpu_list
+df["keys"] = keys_list
+
+sns.lineplot(
+    x="epochs",  # Horizontal axis
+    y="valid_time",  # Vertical axis
+    hue="keys",  # Set color
+    data=df,  # Data source
+    estimator=None,
+    lw=1,
+    sort=True,
+    # style="x",
+    #    style="ngpu",
+    #    kind="line",
+    dashes=False,
+)
+
+# Set title
+plt.title("Validation times accross epochs.")
+
+# Set x-axis label
+plt.xlabel("epochs")
+
+# Set y-axis label
+plt.ylabel("time")
+
+plt.savefig("val_time.png")
+# It can be plt.show() but it requires tkinter.
